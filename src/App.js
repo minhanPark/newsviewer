99b6d0b45b74ba7feef2c51b71536938bf1d2a18
unknown <alsgks1218@naver.com>
2020-02-03 17:52:19 +0900

    add NewsItem, NewsList, Categories Component

--- src/App.js
+++ src/App.js
@@ -1,47 +1,25 @@
-import React from "react";
-import { Route, Link, Switch } from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
-import Profiles from "./Profiles";
-import HistorySample from "./HistorySample";
+import React, { useState } from "react";
+import axios from "axios";
+import NewsList from "./components/NewsList";
+import Categories from "./components/Categories";
 
 function App() {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        "https://newsapi.org/v2/top-headlines?country=kr&apiKey=c2b0496873fe4f9c8a54ec24009b07e5"
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
-    <div className="App">
-      <ul>
-        <li>
-          <Link to="/">홈</Link>
-        </li>
-        <li>
-          <Link to="/about">소개</Link>
-        </li>
-        <li>
-          <Link to="/info">정보</Link>
-        </li>
-        <li>
-          <Link to="/profiles">프로필</Link>
-        </li>
-        <li>
-          <Link to="/history">History 예제</Link>
-        </li>
-      </ul>
-      <hr />
-      <Switch>
-        <Route path="/" component={Home} exact={true} />
-        <Route path={["/about", "/info"]} component={About} />
-        {/* path에 배열을 전달해서 여러 경로에서 같은 컴포넌트를 보여줄 수 있음*/}
-        <Route path="/profiles" component={Profiles} />
-        <Route path="/history" component={HistorySample} />
-        <Route
-          render={({ location }) => (
-            <div>
-              <h2>이 페이지는 존재하지 않습니다.</h2>
-              <p>{location.pathname}</p>
-            </div>
-          )}
-        />
-      </Switch>
-    </div>
+    <>
+      <Categories />
+      <NewsList />
+    </>
   );
 }
 
